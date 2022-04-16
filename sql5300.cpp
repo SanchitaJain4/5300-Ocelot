faeb903d0f43a6d3292b8ddc9e2b4c6d2afb52ef
Alex Larsen <alarsen@seattleu.edu>
2022-04-16 14:23:59 -0700

    final rev to satisfy Milestone1

--- sql5300.cpp
+++ sql5300.cpp
@@ -1,7 +1,8 @@
 /*
   SQLParser.cpp
 
-  SQL statement parser that prints the parse tree of a given SQL statement
+  SQL statement parser that prints the parse tree of a given SQL statement.
+  Referenced sqlHelper.cpp for implementation.
 
   Authors: Alex Larsen alarsen@seattleu.edu,
            Yao Yao yyao@seattleu.edu
@@ -17,11 +18,17 @@ using namespace std;
 using namespace hsql;
 
 string execute(const SQLStatement* parseTree);
+string parseSelect(SelectStatement* stmt);
+string parseCreate(CreateStatement* stmt);
 string parseExpression(Expr* expr);
 string parseOperator(Expr* expr);
 string parseTableRef(TableRef* table);
 string columnDefToString(ColumnDefinition* col);
 
+
+// User input loop program
+// Takes user input and prints the converted parse tree
+// The user must supply the BerkelyDB environment path as a parameter
 int main(int argc, char* argv[]) {
 	
 	// Invalid usage
@@ -41,28 +48,28 @@ int main(int argc, char* argv[]) {
 		cerr << "Exception when opening database environment" << endl;
 		exit(-1);
 	}
-    
+
     // Begin the user input loop
     while (true) 
-    {
-        cout << "SQL> ";
+    {	
+		cout << "SQL> ";
         string input;
-        cin >> input;
-        cout << endl;
+        getline(cin, input);
 
         // End loop if user specifies "break"
         if (input == "quit")
             break;
-		
-		// Skip blank lines
+        
+        // Skip blank lines
         if (input.length() < 1)
             continue;
-		
-		// Parse the response with SQLParser's ParseSQLString function
-		SQLParserResult *result = SQLParser::parseSQLString(input);
 
+        // Parse the response with SQLParser's ParseSQLString function
+        SQLParserResult *result = SQLParser::parseSQLString(input);
+
+        // If valid, parse the statement
 		if (!result->isValid())
-			cout << "invalid SQL: " << input << endl;
+			cout << "Invalid SQL: " << input << endl;
 		else {
 			for (long unsigned int i = 0; i < result->size(); i++) {	
 				cout << execute(result->getStatement(i)) << endl;
@@ -74,152 +81,248 @@ int main(int argc, char* argv[]) {
 }
 
 
+// Parses the parseTree parameter into a valid SQL statement and returns it
+// TODO: Implement insert, import, and show
 string execute(const SQLStatement* parseTree) {
     string result = "";
     
     switch (parseTree->type()) {
-		case kStmtCreate: {
-			CreateStatement* createSmt = (CreateStatement*)parseTree;
-            result += "CREATE TABLE ";
-			result += string(createSmt->tableName);
-			if (createSmt->columns != nullptr) {
-				result += " (";
-				for (ColumnDefinition* col: *createSmt->columns) {
-					result += columnDefToString(col);
-					result += ", ";
-				}
-				result.resize(result.size() - 2);
-				result += ")";
-			}
+        case kStmtSelect: {
+            result += parseSelect((SelectStatement*)parseTree);
             break;
 		}
-        case kStmtSelect: {
-            SelectStatement* selectsmt = (SelectStatement*)parseTree;
-            result += "SELECT ";
-			for(long unsigned int i = 0; i < selectsmt->selectList->size(); i++) {
-				result += parseExpression(selectsmt->selectList->at(i));
-				if(i < (selectsmt->selectList->size() - 1)) {
-					result += ", ";
-				}
-			}
-            result += " FROM ";
-            result += parseTableRef(selectsmt->fromTable);
-			if (selectsmt->whereClause != NULL) {
-				result += " WHERE ";
-				result += parseExpression(selectsmt->whereClause);
-			}
+		case kStmtCreate: {
+			result += parseCreate((CreateStatement*)parseTree);
             break;
 		}
         default:
-            result = "Not implemented";
+            result = "Statement type not implemented: ";
+            result += parseTree->type();
     }
 
     return result;
 }
 
+// Parses a valid SQL select statement
+string parseSelect(SelectStatement* stmt) {
+    string result = "SELECT ";
+
+    // Loop through each statement
+    for(long unsigned int i = 0; i < stmt->selectList->size(); i++) {
+        result += parseExpression(stmt->selectList->at(i));
+        // Create a comma separated list
+        if(i < (stmt->selectList->size() - 1)) {
+            result += ", ";
+        }
+    }
+
+    // Parse the from table
+    result += " FROM ";
+    result += parseTableRef(stmt->fromTable);
+
+    // Parse the where clause expression
+    if (stmt->whereClause != NULL) {
+        result += " WHERE ";
+        result += parseExpression(stmt->whereClause);
+    }
+    
+    return result;
+}
+
+// Parses a valid SQL create statement
+string parseCreate(CreateStatement* stmt) {
+    string result = "CREATE TABLE ";
+
+    result += string(stmt->tableName);
+
+    // Get a comma separated list of the columns
+    if (stmt->columns != nullptr) {
+        result += " (";
+        for (ColumnDefinition* col: *stmt->columns) {
+            result += columnDefToString(col);
+            result += ", ";
+        }
+        result.resize(result.size() - 2);
+        result += ")";
+    }
+    
+    return result;
+}
+
+// Parses a SQL expression
 string parseExpression(Expr* expr) {
+    
+    // Return a blank string if the expression is null
     if(!expr)
 		return "";
 
-    string str = "";
+    string result = "";
 
     switch(expr->type) {
         case kExprStar:
-            str += "*";
+            result += "*";
             break;
         case kExprColumnRef:
             if(expr->table != nullptr) {
-                str += expr->table;
-				str += ".";
+                result += expr->table;
+				result += ".";
             }
-            str += expr->name;
+            result += expr->name;
+            break;
+        case kExprLiteralFloat:
+            result += to_string(expr->fval);
+            break;
+        case kExprLiteralInt:
+            result += to_string(expr->ival);
+            break;
+        case kExprLiteralString:
+            result += string(expr->name);
+            break;
+        case kExprFunctionRef:
+            result += string(expr->name);
+            result += string(expr->expr->name);
             break;
         case kExprOperator:
             if(expr == nullptr) {
-                str += "null ";
+                result += "NULL ";
             }
             else {
-                str += parseOperator(expr) + " ";
+                // Parse the lefthand side of the expression
+                if (expr->expr != nullptr)
+                    result += parseExpression(expr->expr) + " ";
+                else
+                    result += "NULL ";
+                // Parse the operator
+                result += parseOperator(expr) + " ";
+                // Parse the righthand side of the expression
                 if(expr->expr2 != nullptr) {
-                    str += parseExpression(expr->expr2);
+                    result += parseExpression(expr->expr2);
                 }
                 else if(expr->exprList != nullptr) {
                     for(Expr* e : *expr->exprList) {
-                        str += parseExpression(e);
+                        result += parseExpression(e);
                     }
                 }
             }
             break;
         default:
-            cerr << "Expression type not found: " << expr->type << endl;
+            cerr << "Unrecognized expression type: " << expr->type << endl;
     }
 
-    return str;
+    return result;
 }
 
+// Parses an operator as part of a SQL expression
 string parseOperator(Expr* expr)
 {
-	string str;
+	string result;
 	
 	if (expr == nullptr)
 		return "null";
 	
-	if (expr->expr != nullptr)
-		str += parseExpression(expr->expr) + " ";
-	else
-		str += "null ";
-	
 	switch (expr->opType) {
 		case Expr::SIMPLE_OP:
-			str += expr->opChar;
+			result += expr->opChar;
 			break;
 		case Expr::AND:
-			str += "AND";
+			result += "AND";
 			break;
 		case Expr::OR:
-			str += "OR";
+			result += "OR";
 			break;
 		case Expr::NOT:
-			str += "NOT";
+			result += "NOT";
 			break;
 		default:
-			str += expr->opType;
+			result += expr->opType;
 			break;
 	}
-	
-	if (expr->expr2 != nullptr)
-		str += " " + parseExpression(expr->expr2);
-	else
-		str += " null";
-	
-	return str;
+
+	return result;
 }
 
+// Parses a SQL table reference
 string parseTableRef(TableRef* table)
 {
-	//TODO
-	return "NOTIMPL";
+	string result;
+    
+    switch (table->type) {
+        // Parse the name of the table
+        case kTableName:
+            result += table->name;
+            break;
+        // Parse a select statement placed on the table
+        case kTableSelect:
+            result += parseSelect(table->select);
+            break;          
+        // Parse a join
+        case kTableJoin:
+            result += parseTableRef(table->join->left);
+            switch (table->join->type) {
+                case kJoinInner:
+                    result += " JOIN ";
+                    break;
+                case kJoinOuter:
+                    result += " OUTER JOIN ";
+                    break;
+                case kJoinLeft:
+                    result += " LEFT JOIN ";
+                    break;
+                case kJoinRight:
+                    result += " RIGHT JOIN ";
+                    break;
+                default:
+                    result += " ? "; // Not recognized/implemented
+                    break;
+            }
+            
+            result += parseTableRef(table->join->right);
+
+            if (table->join->condition != nullptr)
+            {
+                result += " ON " + parseExpression(table->join->condition);
+            }
+            break;
+        // Parses a cross product between tables
+        case kTableCrossProduct:
+            for (TableRef* tbl : *table->list) {
+                result += parseTableRef(tbl);
+                result += ", ";
+            }
+            result.resize(result.size() - 2);
+            break;
+    }
+
+    // If the table uses an alias, parse it
+    if (table->alias != nullptr)
+    {
+        result += " AS ";
+        result += table->alias;
+    }
+
+    return result;
 }
 
+// Converts a ColumnDefinition variable to a string
 string columnDefToString(ColumnDefinition* col)
 {
-	string str;
-	str += col->name;
+	string result;
+	result += col->name;
 	
 	switch (col->type) {
 		case ColumnDefinition::DOUBLE:
-			str += " DOUBLE";
+			result += " DOUBLE";
 			break;
 		case ColumnDefinition::INT:
-			str += " INT";
+			result += " INT";
 			break;
 		case ColumnDefinition::TEXT:
-			str += " TEXT";
+			result += " TEXT";
 			break;
 		default:
-			str += " ?";
+			result += " ?";
 			break;
 	}
-	
-	return str;
+
+	return result;
 }
